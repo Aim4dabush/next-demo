0233f6b1951ecbeace06a44503e4f08994dbe436
aim4dabush <edarasato@gmail.com>
2022-10-18 11:29:25 -0700

    added useRouter to product detail page

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
+
 const ProductDetailsPage = () => {
+  const router = useRouter();
+
+  console.log(router.query.productId);
+
   return <div>Product Detail Page</div>;
 };
 
