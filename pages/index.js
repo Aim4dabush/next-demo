cab797ef4318be31db853a62dc4dbe1b8aa60305
aim4dabush <edarasato@gmail.com>
2022-10-18 10:45:20 -0700

    added products page and designed home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+//components
+import Link from "next/link";
+
+//styles
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h1>Enjoy Your Shopping Experience</h1>
+        <Link href="/products">
+          <button className={styles.button}>Start Shopping</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
